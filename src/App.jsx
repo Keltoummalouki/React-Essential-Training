e72c0953a8ae5a852d241a26f0a8ba46e1c28248
Keltoummalouki <keltoummalouki911@gmail.com>
2025-03-09 05:38:43 +0000

    Adding component properties

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,20 @@ let lastname = "Malouki";
 let currentyear = 2025;
 let mybirthyear = 2006;
 let country = "Morocco";
-let city
+let city = "Casablanca"
 
 function Address() {
-  return <h2> I live in {country} exactly in {city} </h2>
+  return <h4> I live in {country} exactly in {city} </h4>
+}
+
+function Prop(props){
+  return <h5>I'm a Full Stack {props.job}</h5>
 }
 
 function App() {
     return (
         <h1>Hello, My name is {firstname} {lastname.toUpperCase()}, I'm {currentyear-mybirthyear}
-        <Address /> </h1>
+        <Address /> <Prop job ="Devlopper"/></h1>
     ) 
 }
 export default App
