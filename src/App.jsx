7798c55669f9dcf1e6c7c72f67163b610fdf24b4
Keltoummalouki <keltoummalouki911@gmail.com>
2025-03-09 05:16:26 +0000

    Working with JSX syntax

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css'
 
+let firstname = "Keltoum";
+let lastname = "Malouki";
+let currentyear = 2025;
+let mybirthyear = 2006;
+
 function App() {
-  return <h1>Hello World!</h1>
+  return <h1>Hello, My name is {firstname} {lastname.toUpperCase()}, I'm {currentyear-mybirthyear}</h1>
 }
 
 export default App
