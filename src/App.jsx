53650a020bd38b9cc4b0dcde25c5ad60ff9596de
Keltoummalouki <keltoummalouki911@gmail.com>
2025-03-09 05:31:33 +0000

    Creating a React component

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,17 @@ let firstname = "Keltoum";
 let lastname = "Malouki";
 let currentyear = 2025;
 let mybirthyear = 2006;
+let country = "Morocco";
+let city
 
-function App() {
-  return <h1>Hello, My name is {firstname} {lastname.toUpperCase()}, I'm {currentyear-mybirthyear}</h1>
+function Address() {
+  return <h2> I live in {country} exactly in {city} </h2>
 }
 
+function App() {
+    return (
+        <h1>Hello, My name is {firstname} {lastname.toUpperCase()}, I'm {currentyear-mybirthyear}
+        <Address /> </h1>
+    ) 
+}
 export default App
